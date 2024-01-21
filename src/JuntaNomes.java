7f5177e5946b55e31f6578988a771da304bada44
dropperDEV <jamescosta4747@Gmail.com>
2024-01-21 16:44:33 -0300

    feat: function concat, class String output system

---
+++ src/JuntaNomes.java
@@ -0,0 +1,12 @@
+public class JuntaNomes {
+
+    public static void main(String [] args){
+        String Nome = "Robertin";
+        String Vulgo =  "Do Gelo";
+        System.out.println(concaNome(Nome,Vulgo));
+    }
+
+    public static String concaNome(String pNome, String sNome){
+        return pNome.concat(" ").concat(sNome);
+    }
+}
