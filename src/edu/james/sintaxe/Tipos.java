dc24e460ebfa870eec2be40bd5bc79df142c0224
dropperDEV <jamescosta4747@Gmail.com>
2024-01-22 00:36:18 -0300

    feat: function concat, class String output system

---
+++ src/edu/james/sintaxe/Tipos.java
@@ -0,0 +1,12 @@
+package edu.james.sintaxe;
+
+public class Tipos {
+    public static void main(String [] args){
+        // INTEGER NUMBERS  // byte -128 a 127 (8 bits), short -2^15 a 2^15-1 (16 bits), int -2^31 a 2^31-1 (32 bits), long -2^63 a 2^63-1 (64 bits)
+        // DECIMAL NUMBERS // double, float
+        // LETTERS AND WORDS // char, String (Class)
+
+        // float nome = 44.24F
+        // long nome = 414141L
+    }
+}
