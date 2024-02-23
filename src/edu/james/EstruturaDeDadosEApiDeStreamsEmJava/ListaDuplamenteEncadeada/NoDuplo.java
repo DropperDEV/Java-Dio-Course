8f8bee21daacebd2fa2fb0f57bff192b71b7a10a
dropperDEV <jamescosta4747@Gmail.com>
2024-02-23 15:20:56 -0300

    feat: add NoDuplo in ListaDuplamenteEncadeada

---
+++ src/edu/james/EstruturaDeDadosEApiDeStreamsEmJava/ListaDuplamenteEncadeada/NoDuplo.java
@@ -0,0 +1,42 @@
+package edu.james.EstruturaDeDadosEApiDeStreamsEmJava.ListaDuplamenteEncadeada;
+
+public class NoDuplo<T> {
+    private T conteudo;
+    private NoDuplo<T> noPrevio;
+    private NoDuplo<T> noProximo;
+
+    public NoDuplo(T conteudo){
+        this.conteudo = conteudo;
+    }
+
+    public T getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(T conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public NoDuplo<T> getNoPrevio() {
+        return noPrevio;
+    }
+
+    public void setNoPrevio(NoDuplo<T> noPrevio) {
+        this.noPrevio = noPrevio;
+    }
+
+    public NoDuplo<T> getNoProximo() {
+        return noProximo;
+    }
+
+    public void setNoProximo(NoDuplo<T> noProximo) {
+        this.noProximo = noProximo;
+    }
+
+    @Override
+    public String toString() {
+        return "NoDuplo{" +
+                "conteudo=" + conteudo +
+                '}';
+    }
+}
