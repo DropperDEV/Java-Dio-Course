dece285d1c90d20e99d9893caebd9540cfdc8061
dropperDEV <jamescosta4747@Gmail.com>
2024-02-24 14:49:16 -0300

    feat: add getNo() method in ListaCircular

--- src/edu/james/EstruturaDeDadosEApiDeStreamsEmJava/ListaCirculares/ListaCircular.java
+++ src/edu/james/EstruturaDeDadosEApiDeStreamsEmJava/ListaCirculares/ListaCircular.java
@@ -6,6 +6,19 @@ public class ListaCircular<T>{
     private int tamanhoLista;
 
 
+    private No<T> getNo(int index){
+        if(this.isEmpty()){
+            throw new IndexOutOfBoundsException(("A lista está vazia"));
+        }
+        if(index == 0 ){
+            return  this.cauda;
+        }
+        No<T> noAuxiliar = this.cauda;
+        for (int i = 0;(i < index) && (noAuxiliar != null);i++){
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return  noAuxiliar;
+    }
     public boolean isEmpty(){
         return this.tamanhoLista == 0 ? true : false;
     }
