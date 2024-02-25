5bde9b4d5c655a0a0c58448a5d7d4f96644aee1b
dropperDEV <jamescosta4747@Gmail.com>
2024-02-25 19:52:50 -0300

    feat: add ObjArvore Class in model

--- src/edu/james/EstruturaDeDadosEApiDeStreamsEmJava/ArvoreBinaria/model/ObjArvore.java
+++ src/edu/james/EstruturaDeDadosEApiDeStreamsEmJava/ArvoreBinaria/model/ObjArvore.java
@@ -1,4 +1,8 @@
 package edu.james.EstruturaDeDadosEApiDeStreamsEmJava.ArvoreBinaria.model;
 
-public class ObjArvore {
+public  abstract class  ObjArvore<T> implements  Comparable<T> {
+    public abstract boolean equals(Object o);
+    public abstract int hashCode();
+    public abstract int compareTo(T outro);
+    public abstract String toString();
 }
